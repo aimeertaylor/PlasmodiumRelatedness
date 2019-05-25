2544bda0126cc9a18c646d3f8e8f2965dc9d5c2b
artaylor85 <aimee.r.taylor.85@gmail.com>
2019-05-25 09:18:16 -0400

    Edit comments, added plot

--- Code/Extract_typical_kfixed.R
+++ Code/Extract_typical_kfixed.R
@@ -1,10 +1,12 @@
 #====================================
 # Extracting a typical k for r=0.5 
-# 12 based on Thai data set
+# 8 based on Thai data set
 #====================================
 rm(list = ls())
 load('../RData/TM_WGS_mles.RData')
 rhats = mle_df$rhat_hmm
 ind <- which(rhats < 0.525 & rhats > 0.475)
-mean(mle_df$khat_hmm[ind])
+mean(mle_df$khat_hmm[ind]) # 8
+range(mle_df$khat_hmm[ind])
 plot(mle_df$khat_hmm[ind], mle_df$rhat_hmm[ind])
+plot(y = mle_df$khat_hmm, x = mle_df$rhat_hmm, ylim = c(0,50))
